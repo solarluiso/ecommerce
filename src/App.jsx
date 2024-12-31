72447fc3f13d01170de6a7ece50319e619e4a795
Luis <solarluiso@gmail.com>
2024-12-31 08:30:01 -0500

    add StoreContext for global state management and wrap App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 import { Route, Routes } from "react-router-dom";
 
 import NavBar from "./components/NavBar";
-
 import Home from "./views/Home";
 import ProductDetail from "./views/ProductDetail";
 import Favorites from "./views/Favorites";
+import StoreContextProvider from "./context/StoreContext";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <NavBar></NavBar>
-      <main className="max-w-7xl mx-auto py-6">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/favorites" element={<Favorites />} />
-        </Routes>
-      </main>
-    </div>
+    <StoreContextProvider>
+      <div className="min-h-screen bg-gray-100">
+        <NavBar></NavBar>
+        <main className="max-w-7xl mx-auto py-6">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </main>
+      </div>
+    </StoreContextProvider>
   );
 }
 
